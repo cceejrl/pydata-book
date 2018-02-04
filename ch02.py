e750cbcaec8630f085cb77cfb2b2ba9e73927262
cceejrl <425365990@qq.com>
2018-02-04 21:26:18 +0800

    Learning python data in step by step.

--- ch02.py
+++ ch02.py
@@ -6,16 +6,16 @@ import json
 from collections import defaultdict
 from collections import Counter
 from pandas import DataFrame, Series
-import pandas as pd
+# import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import timeit
 
 start = timeit.timeit()
 
 path = "ch02/usagov_bitly_data2012-03-16-1331923249.txt"
 
-print(path)
+# print(path)
 
 # records = []
 
@@ -25,7 +25,7 @@ print(path)
 # 掌握此列表推导式写法
 records = [json.loads(line) for line in open(path)]
 
-print(records[0]['tz'])
+# print(records[0]['tz'])
 
 time_zones = [rec['tz'] for rec in records if 'tz' in rec]
 
@@ -61,8 +61,8 @@ my_counts_std = Counter(time_zones)
 
 top_cnt = top_counts(my_counts)
 
-print(top_cnt)
-print(my_counts_std.most_common(10))
+# print(top_cnt)
+# print(my_counts_std.most_common(10))
 
 frame = DataFrame(records)
 # tz_counts = frame['tz'].value_counts()
@@ -70,12 +70,26 @@ clean_tz = frame['tz'].fillna('Missing')  # 填充空白
 clean_tz[clean_tz == ''] = 'Unknown'
 tz_counts = clean_tz.value_counts()
 
-print(tz_counts[:10])
+# print(tz_counts[:10])
 tz_counts[:10].plot(kind='barh', rot=0)  # 不使用IPython，在Pycharm中单步走可显示图像，直接运行太短暂看不到
 
 rlt = Series([x.split()[0] for x in frame.a.dropna()])
-print(rlt[:5])
-print(rlt.value_counts()[:8])
+# print(rlt[:5])
+# print(rlt.value_counts()[:8])
+
+cframe = frame[frame.a.notnull()]
+operating_sys = np.where(cframe['a'].str.contains('Windows'), 'Wdinows', 'Not Windows')
+# print(operating_sys[:5])
+
+by_tz_os = cframe.groupby(['tz', operating_sys])
+agg_counts = by_tz_os.size().unstack().fillna(0)
+print(agg_counts[:10])
+
+indexer = agg_counts.sum(1).argsort()
+print(indexer[:10])
+
+count_subset = agg_counts.take(indexer)[-10:]
+print(count_subset)
 
 end = timeit.timeit()
 print("It takes ", end - start, "second(s)")
